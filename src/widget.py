6a2d72fb85dcd84f51ec509a8dee08237a974e0c
Dmitry <mitya.200@mail.ru>
2024-11-28 01:00:58 +0300

    lesson two

--- src/widget.py
+++ src/widget.py
@@ -1,11 +1,15 @@
 from src.masks import get_mask_card_number, get_mask_account
+#Добавляю функции из модуля masks.py
 
-
-def mask_account_card(account_card):
+def mask_account_card(account_card: str) -> str:
+    #Функция которая умеет обрабатывать информацию как о картах, так и о счетах
+    #С помощью функций из прошлого урока
     if account_card[:4] == "Счет":
         return f"{account_card[:-21]} {get_mask_account(account_card[-20:])}"
     else:
         return f"{account_card[:-17]} {get_mask_card_number(account_card[-16:])}"
+
+
 print(mask_account_card("Maestro 1596837868705199"))
 print(mask_account_card("Счет 64686473678894779589"))
 print(mask_account_card("MasterCard 7158300734726758"))
@@ -16,8 +20,10 @@ print(mask_account_card("Visa Gold 5999414228426353"))
 print(mask_account_card("Счет 73654108430135874305"))
 
 
-def get_date(date_string):
+def get_date(date_string: str) -> str:
+    # Функция для вывода даты
     date_parts = date_string.split("T")
     return date_parts[0].split("-")[2] + "." + date_parts[0].split("-")[1] + "." + date_parts[0].split("-")[0]
+
 
 print(get_date("2024-03-11T02:26:18.671407"))
